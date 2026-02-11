473884bf5fabd476f98e51564a7ae25dc3d626c1
erichategekimana <h.eric@alustudent.com>
2026-02-11 23:12:09 +0200

    init.py file added

--- sms_system/app/__init__.py
+++ sms_system/app/__init__.py
@@ -7,4 +7,13 @@ from .database import db, init_db
 
 migrate = Migrate()
 
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
 
+    # Initialize database
+    init_db(app)
+    migrate.init_app(app, db)
+
+    # Enabling CORS
+    CORS(app, resouces={r"/api/*": {"origins": "*"}})
